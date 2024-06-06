7cdd2b646e7a53ffba05a4c2129378236a8c59f1
Voltstro <me@voltstro.dev>
2024-06-06 23:05:34 +1000

    Prevent editing of products on customer login Increase default column widths

--- client/src/main/java/au/edu/cqu/jhle/controllers/ProductsController.java
+++ client/src/main/java/au/edu/cqu/jhle/controllers/ProductsController.java
@@ -11,12 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import au.edu.cqu.jhle.shared.models.User;
 import au.edu.cqu.jhle.shared.requests.GetProductsRequest;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -41,8 +43,13 @@ public class ProductsController implements Initializable {
     
     @FXML
     private TableColumn<Product, Double> unitPriceColumn;
+
+    @FXML
+    private Button newProductBtn;
     
     private List<Product> productsList = new ArrayList<>();
+
+    private boolean isCustomer;
 
     /**
      * Initializes the controller class.
@@ -50,6 +57,12 @@ public class ProductsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ClientRequestManager requestManager = ClientApp.getClientRequestManager();
+        User user = requestManager.getLoggedInUser();
+        if(user.getRoleId() == 1) {
+            newProductBtn.setDisable(true);
+            isCustomer = true;
+        }
+
         try {
             GetProductsRequest  getProductsRequest = requestManager.getProductsRequest(new GetProductsRequest());
             productsList = getProductsRequest.getProductList();
@@ -109,6 +122,8 @@ public class ProductsController implements Initializable {
     }
     
     private void openProductDetailsPage(Product product) throws IOException {
+        if(isCustomer) return;
+
         //open product details
         ProductDetailController controller = ClientApp.setRoot("productDetail");
         //set selected product
